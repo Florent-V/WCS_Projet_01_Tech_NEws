59b1c031ab7c3756a71c6069e844bb10ae5c30c4
Florent-Vasseur <vasseurflorent@gmail.com>
2022-09-27 20:46:31 +0200

    v1 header & footer ok

--- index.php
+++ index.php
@@ -5,19 +5,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tech News to Watch</title>
+    <link rel="shortcut icon" href="./img/logo.png" type="image/x-icon">
     <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.2/css/all.css" />
 </head>
 <body>
     <header>
-    
-
+        <img src="./img/logo.png" alt="Logo du site">
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Articles</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Contact Us</a></li>
+            </ul>
+        </nav>
 
     </header>
     <main>
 
     </main>
     <footer>
-
+        <p>Copyright &copy <?php echo date('Y'); ?> <i class="fa-solid fa-bolt"></i> PHPower <i class="fa-solid fa-bolt"></i></p>
+        <p><i class="fa-brands fa-square-instagram"></i><i class="fa-brands fa-square-facebook"></i><i class="fa-brands fa-linkedin"></i><i class="fa-brands fa-square-twitter"></i></p>
     </footer>
     
 </body>
