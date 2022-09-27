716446e76aacc40f05be6367f8326e8e7b5b7f72
Florent-Vasseur <vasseurflorent@gmail.com>
2022-09-27 16:46:31 +0200

    Commit initial début construction header

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # WCS_Projet_01_Tech_NEws
 Projet HTML/CSS de site Tech News
+
+## WireFrame
+
+https://excalidraw.com/#json=FoG-DzlXA3CnwrMeaQzKY,0chBPrZA0Yo99rsIi-PO6g
+
+## Répartion des rôles 
+
+Jessica : Page Article. Création d'un Trello pour le groupe
+
+Maxence : Page About Us
+
+Alexandre : Page contact
+
+Florent : Page Accueil
+
+A définir : page article/Comment bien réaliser sa veille tech
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tech News to Watch</title>
+    <link rel="stylesheet" href="./style/style.css">
+</head>
+<body>
+    <header>
+        <img src="./img/logo.png" alt="Logo du site">
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Articles</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Contact Us</a></li>
+                <li><a href="">oops</a></li>
+            </ul>
+        </nav>
+
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+    
+</body>
+</html>
