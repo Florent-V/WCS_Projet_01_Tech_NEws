846d47aa389ffc45b97a7e55ab72c7c13bde8083
Florent-Vasseur <vasseurflorent@gmail.com>
2022-09-27 16:48:18 +0200

    commit initial arborescence

--- index.php
+++ index.php
@@ -9,16 +9,8 @@
 </head>
 <body>
     <header>
-        <img src="./img/logo.png" alt="Logo du site">
-        <nav>
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Articles</a></li>
-                <li><a href="">About Us</a></li>
-                <li><a href="">Contact Us</a></li>
-                <li><a href="">oops</a></li>
-            </ul>
-        </nav>
+    
+
 
     </header>
     <main>
