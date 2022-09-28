b05685b16d233591a7c79ea001c8c0e93c04fa15
Alexandre Delmotte <alex.delmotte89@gmail.com>
2022-09-28 17:09:49 +0200

    sauvegarde

--- contact_us.php
+++ contact_us.php
@@ -6,18 +6,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tech News to Watch</title>
     <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="./style/contact.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.2/css/all.css" />
+
 </head>
 <body>
-    <header>
+<i class="fa-solid fa-phone"></i>
+<?php 
+include '_header.php';
+?>
+<main>
+  <section class="bandeau"></section>
 
+  <section class="boxFormulaire">
+    <form>                        
+      <h3>Contact us</h3>
 
-    </header>
-    <main>
+      <div class="formulaire">
+        <input type="email" placeholder="@" />
 
-    </main>
-    <footer>
+        <input type="text" placeholder="" />
 
-    </footer>
-    
+        <input type="text" placeholder="" />
+
+        <textarea placeholder="Your message"></textarea>
+      </div> 
+    </form>
+  </section>
+
+</main>
+  <?php include 
+     '_footer.php';
+  ?>
 </body>
 </html>
