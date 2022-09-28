4f8f1cb515e6dc869fb4d7697221ae334782023d
Pecheux Jessica <98033277+jessicapecheux@users.noreply.github.com>
2022-09-28 16:54:04 +0200

    creation fichier publication.php

--- publication.php
+++ publication.php
@@ -4,26 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tech News to Watch</title>
     <link rel="shortcut icon" href="./img/logo.png" type="image/x-icon">
-    <link rel="stylesheet" href="./style/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.2/css/all.css" />
-    <link rel="stylesheet" href="publication.css">
+    <title>Tech News to Watch</title>
+   <link rel="stylesheet" href="./style/style.css">
+   <link rel="stylesheet" href="./style/publication.css">
 </head>
-<body>
-    <?php 
+
+    
+</body>
+
+
+
+
+
+
+</div>
+    
+<?php 
         include '_header.php';
     ?>
-    
-    
-    <main>
 
-    </main>
-
-    <?php 
+<?php 
         include '_footer.php';
     ?>
     
     
-</body>
+
+    
+    
+    </body> 
+    
 </html>
