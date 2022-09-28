7af6654becdb9b9fa0ace50d3d2fb1e9ea267fb2
MaxenceCodeFR <maxence.hattabi@gmail.com>
2022-09-28 14:41:11 +0200

    some changes about aboutus page

--- _header.php
+++ _header.php
@@ -1,12 +1,12 @@
 <header>
-        <img src="./img/logo.png" alt="Logo du site">
-        <nav>
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Articles</a></li>
-                <li><a href="">About Us</a></li>
-                <li><a href="">Contact Us</a></li>
-            </ul>
-        </nav>
+    <img src="./img/logo.png" alt="Logo du site">
+    <nav>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Articles</a></li>
+            <li><a href="about_us.php">About Us</a></li>
+            <li><a href="">Contact Us</a></li>
+        </ul>
+    </nav>
 
 </header>
--- about_us.php
+++ about_us.php
@@ -1,23 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tech News to Watch</title>
-    <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="./style/style_aboutus.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.2/css/all.css" />
 </head>
+
 <body>
     <header>
-
-
+        <?php
+        include '_header.php';
+        ?>
     </header>
     <main>
+        <section class="florentv">
+            <h1>Florent Vasseur</h1>
+            <img src="./img/profile_pic_flo.png" alt="Florent Vasseur" />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt magna lacus, et feugiat turpis tempus id. Nulla vestibulum, nibh ac blandit laoreet, massa leo suscipit quam, et dictum purus urna sit amet velit. Quisque sed eros imperdiet, aliquet tellus scelerisque, maximus metus. Etiam eget nisl in neque porttitor finibus. Aenean consectetur nunc sem, vitae porta ipsum cursus eu. Fusce nisi lectus, finibus id dui sed, pulvinar tempus mauris. Duis ut ultricies lorem. Curabitur congue dolor ante, quis interdum nisi sagittis quis. Suspendisse tempor, dui nec commodo consectetur, libero justo molestie nunc, id sodales enim purus lacinia est. Fusce cursus orci nec enim egestas molestie. Donec nec hendrerit metus.</p>
+        </section>
+
+        <section class="jessicap">
+            <h1>Jessica Pêcheux</h1>
+            <img src="./img/profile_pic_jess.png" alt="Jessica Pêcheux" />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt magna lacus, et feugiat turpis tempus id. Nulla vestibulum, nibh ac blandit laoreet, massa leo suscipit quam, et dictum purus urna sit amet velit. Quisque sed eros imperdiet, aliquet tellus scelerisque, maximus metus. Etiam eget nisl in neque porttitor finibus. Aenean consectetur nunc sem, vitae porta ipsum cursus eu. Fusce nisi lectus, finibus id dui sed, pulvinar tempus mauris. Duis ut ultricies lorem. Curabitur congue dolor ante, quis interdum nisi sagittis quis. Suspendisse tempor, dui nec commodo consectetur, libero justo molestie nunc, id sodales enim purus lacinia est. Fusce cursus orci nec enim egestas molestie. Donec nec hendrerit metus.</p>
+        </section>
+
+        <section class="alexandred">
+            <h1>Alexandre Delmotte</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt magna lacus, et feugiat turpis tempus id. Nulla vestibulum, nibh ac blandit laoreet, massa leo suscipit quam, et dictum purus urna sit amet velit. Quisque sed eros imperdiet, aliquet tellus scelerisque, maximus metus. Etiam eget nisl in neque porttitor finibus. Aenean consectetur nunc sem, vitae porta ipsum cursus eu. Fusce nisi lectus, finibus id dui sed, pulvinar tempus mauris. Duis ut ultricies lorem. Curabitur congue dolor ante, quis interdum nisi sagittis quis. Suspendisse tempor, dui nec commodo consectetur, libero justo molestie nunc, id sodales enim purus lacinia est. Fusce cursus orci nec enim egestas molestie. Donec nec hendrerit metus.</p>
+        </section>
+
+        <section class="maxenceh">
+            <h1>Maxence Hattabi</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt magna lacus, et feugiat turpis tempus id. Nulla vestibulum, nibh ac blandit laoreet, massa leo suscipit quam, et dictum purus urna sit amet velit. Quisque sed eros imperdiet, aliquet tellus scelerisque, maximus metus. Etiam eget nisl in neque porttitor finibus. Aenean consectetur nunc sem, vitae porta ipsum cursus eu. Fusce nisi lectus, finibus id dui sed, pulvinar tempus mauris. Duis ut ultricies lorem. Curabitur congue dolor ante, quis interdum nisi sagittis quis. Suspendisse tempor, dui nec commodo consectetur, libero justo molestie nunc, id sodales enim purus lacinia est. Fusce cursus orci nec enim egestas molestie. Donec nec hendrerit metus.</p>
+        </section>
 
     </main>
-    <footer>
 
+
+    <footer>
+        <?php
+        include '_footer.php';
+        ?>
     </footer>
-    
+
 </body>
+
 </html>
