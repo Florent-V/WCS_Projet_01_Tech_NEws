0daecfc8de64ed884d322c1b6e2ba3024248a3e2
Florent-Vasseur <vasseurflorent@gmail.com>
2022-09-28 09:43:59 +0200

    header et footer fait

--- index.php
+++ index.php
@@ -10,25 +10,19 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.2/css/all.css" />
 </head>
 <body>
-    <header>
-        <img src="./img/logo.png" alt="Logo du site">
-        <nav>
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Articles</a></li>
-                <li><a href="">About Us</a></li>
-                <li><a href="">Contact Us</a></li>
-            </ul>
-        </nav>
-
-    </header>
+    <?php 
+        include '_header.php';
+    ?>
+    
+    
     <main>
 
     </main>
-    <footer>
-        <p>Copyright &copy <?php echo date('Y'); ?> <i class="fa-solid fa-bolt"></i> PHPower <i class="fa-solid fa-bolt"></i></p>
-        <p><i class="fa-brands fa-square-instagram"></i><i class="fa-brands fa-square-facebook"></i><i class="fa-brands fa-linkedin"></i><i class="fa-brands fa-square-twitter"></i></p>
-    </footer>
+
+    <?php 
+        include '_footer.php';
+    ?>
+    
     
 </body>
 </html>
