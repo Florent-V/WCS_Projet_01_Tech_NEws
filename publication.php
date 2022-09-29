62c70f0a4fedde993bec60aade4bac4356ec1b3b
Pecheux Jessica <98033277+jessicapecheux@users.noreply.github.com>
2022-09-29 10:22:53 +0200

    création article 1

--- publication.php
+++ publication.php
@@ -11,28 +11,42 @@
    <link rel="stylesheet" href="./style/publication.css">
 </head>
 
-    
-</body>
 
-
-
-
-
-
-</div>
-    
-<?php 
+<body>
+    <?php 
         include '_header.php';
     ?>
 
-<?php 
+    <section>
+    <h1>Publication News Tech</h1>
+    <p>Le meilleur de la veille tec</p>
+    </section>
+
+        <h2>Publication PHP</h2>
+
+        <article>
+            <h1>titre article</h1>
+            <p class="descrition">Cet article parle va vous permettre de mieux appréhender symphony. </p>
+            <p class="date"28septembre 2022></p>
+
+
+        </article>
+    
+        
+
+    <main>
+
+    </main>
+    
+    
+    
+
+    
+
+    <?php 
         include '_footer.php';
     ?>
     
     
-
-    
-    
-    </body> 
-    
+</body>
 </html>
