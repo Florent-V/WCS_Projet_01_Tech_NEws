5ae52713be46817185c977d4a6f95b8f87680201
Pecheux Jessica <98033277+jessicapecheux@users.noreply.github.com>
2022-10-04 17:33:14 +0200

    création d'un dossier css dans img sur vs code , ajout d'images , création de page publication

--- publication.php
+++ publication.php
@@ -17,27 +17,208 @@
         include '_header.php';
     ?>
 
-    <section>
-    <h1>Publication News Tech</h1>
-    <p>Le meilleur de la veille tec</p>
-    </section>
 
-        <h2>Publication PHP</h2>
-
-        <article>
-            <h1>titre article</h1>
-            <p class="descrition">Cet article parle va vous permettre de mieux appréhender symphony. </p>
-            <p class="date"28septembre 2022></p>
-
-
-        </article>
+     <main>
     
-        
+     
+ 
+    <h1>Le meilleur de la Veille Tech</h1>
+   
 
-    <main>
 
-    </main>
+    <div class="publicationphp">    
+    <div class="article1"> 
+    <img src="./img/css/symfony.png" alt="image symfony">
+    <p class="descripion">Symfony Docs est l'un des projets PHP les plus réputés. Il s'agit d'un framework full-stack roboste et d'un ensemble de composants réutilisables.</p>
+    <a href="https://symfony.com/doc/current/index.html">Symfony DOCS</a>
+    <p class='social'>
+                <span><i class="fa-regular fa-bookmark"></i></span>
+                <span><i class="fa-regular fa-message"></i> 5 </span>
+                <span><i class="fa-regular fa-thumbs-up"></i> 15 </span>
+                <span><i class="fa-solid fa-share-nodes"></i></span>
+            </p>
+    </div>
+
+<div class="article2">
+    <img src="./img/css/php2.png" alt="logo php">
+    <p class="descripion">Symphony Docs propose de la documentation complète afin de pouvoir prendre en main symphony</p>
+    <a href="https://www.php.net/">Manuel php</a>
+    <p class='social'>
+                <span><i class="fa-regular fa-bookmark"></i></span>
+                <span><i class="fa-regular fa-message"></i> 5 </span>
+                <span><i class="fa-regular fa-thumbs-up"></i> 15 </span>
+                <span><i class="fa-solid fa-share-nodes"></i></span>
+            </p>
+    </div>
+
+<div  class="article3">
+    <img src="./img/css/projet.jpeg" alt="image coder en php"> 
+    <p class="descripion">Cet article propose 8 idées de projets pour progresser en php</p>
+    <a href="https://www.commentcoder.com/projets-php-debutants/">Comment coder?</a>
+    <p class='social'>
+                <span><i class="fa-regular fa-bookmark"></i></span>
+                <span><i class="fa-regular fa-message"></i> 12 </span>
+                <span><i class="fa-regular fa-thumbs-up"></i> 125 </span>
+                <span><i class="fa-solid fa-share-nodes"></i></span>
+            </p>
+    </div>
+
+<div  class="article4">
+    <img src="./img/css/mysql.jpeg" alt="image MySQL">
+    <p class="descripion">Cet article vous explique comment mettre à jour facilement votre version MySQL</p>
+    <a href="https://kinsta.com/fr/blog/version-mysql/">Comment mettre à jours votre version DE MySQL</a>
+    <p class='social'>
+                <span><i class="fa-regular fa-bookmark"></i></span>
+                <span><i class="fa-regular fa-message"></i> 17 </span>
+                <span><i class="fa-regular fa-thumbs-up"></i> 88 </span>
+                <span><i class="fa-solid fa-share-nodes"></i></span>
+            </p>
+    </div>
+
+<div  class="article5">
+    <img src="./img/css/php8.jpeg" alt="image php 8">
+    <p class="descripion">Cette article propose de découvrir les nouveautés de php 8</p>
+    <a href="https://mindsers.blog/fr/post/nouveautes-php8-attributs/">Les nouveautés de php 8 : les atributs</a>
+    <p class='social'>
+                <span><i class="fa-regular fa-bookmark"></i></span>
+                <span><i class="fa-regular fa-message"></i> 76 </span>
+                <span><i class="fa-regular fa-thumbs-up"></i> 57 </span>
+                <span><i class="fa-solid fa-share-nodes"></i></span>
+            </p>
+</div>
+</div>
+
+<div class="publicationjs">  
+<div  class="article6">
+    <img src="./img/css/framework.png" alt="image logo des frameworks JavaScript">
+     <p class="descripion">Découvrez quelques-uns des meilleurs frameworks pour créer une application moderne en moins de temps.</p>
+    <a href="https://geekflare.com/fr/best-javascript-frameworks/">Les 12 meilleurs frameworks JavaScript</a>
+    <p class='social'>
+                <span><i class="fa-regular fa-bookmark"></i></span>
+                <span><i class="fa-regular fa-message"></i> 98 </span>
+                <span><i class="fa-regular fa-thumbs-up"></i> 109 </span>
+                <span><i class="fa-solid fa-share-nodes"></i></span>
+            </p>
+    </div>
+
+<div  class="article7">
+    <img src="./img/css/js3.jpeg" alt="image JavaScript ">
+    <p class="descripion">Cet article vous explique comment développer un lecteur de fichier texte en JavaScript</p>
+    <a href="https://www.journaldunet.fr/web-tech/developpement/1202851-comment-developper-un-lecteur-de-fichier-texte-en-javascript/">Comment développer un lecteur de fichier texte en JavaScript?</a>
+    <p class='social'>
+                <span><i class="fa-regular fa-bookmark"></i></span>
+                <span><i class="fa-regular fa-message"></i> 76 </span>
+                <span><i class="fa-regular fa-thumbs-up"></i> 464 </span>
+                <span><i class="fa-solid fa-share-nodes"></i></span>
+            </p>
+    </div>
+
+<div  class="article8">
+    <img src="./img/css/faceapi.jpeg" alt="image de deux visages">
+    <p class="descripion">Le module JavaScript face-api.js implémente des réseaux de neurones convolutifs pour résoudre la détections et la reconnaissance des visages ainsi que la reconnaissance des repères faciaux. L'api face-api.js exploite TensorFlow.js et est optimisé pour le bureau et le web mobile.</p>
+    <a href="https://www.infoq.com/fr/news/2020/03/face-api-js/">Détection de visage JavaScript avec Face-api.js</a>
+    <p class='social'>
+                <span><i class="fa-regular fa-bookmark"></i></span>
+                <span><i class="fa-regular fa-message"></i> 35 </span>
+                <span><i class="fa-regular fa-thumbs-up"></i> 153 </span>
+                <span><i class="fa-solid fa-share-nodes"></i></span>
+            </p>
+    </div>
     
+<div  class="article9">
+    <img src="./img/css/codejs.jpeg" alt="image de code JavaScrript">
+    <p class="descripion">Fraîchement adoptée, la spécification ECMAScript 2022 adopte une licence plus permissive qui aligne sur celle des standards W3C.</p>
+    <a href="https://www.silicon.fr/javascript-change-licence-alignement-w3c-442343.html">JavaScript change de licence : un alignement sur le W3C</a>
+    <p class='social'>
+                <span><i class="fa-regular fa-bookmark"></i></span>
+                <span><i class="fa-regular fa-message"></i> 52 </span>
+                <span><i class="fa-regular fa-thumbs-up"></i> 198 </span>
+                <span><i class="fa-solid fa-share-nodes"></i></span>
+            </p>
+    </div>
+
+<  <div class="article 10">
+    <img src="./img/css/js2.jpeg" alt="image statistique langage de programmation">
+    <p class="descripion">Une nouvelle vague de l’étude State of JS est parue. À partir de critères de notoriété, d’intérêt, d’usage et de satisfaction, elle dresse un portrait de l’écosystème JavaScript</p>
+    <a href="https://www.silicon.fr/javascript-developpeurs-2022-432946.html">JavaScript : que plébiscitent les développeurs en 2022 ?</a>
+    <p class='social'>
+                <span><i class="fa-regular fa-bookmark"></i></span>
+                <span><i class="fa-regular fa-message"></i> 89 </span>
+                <span><i class="fa-regular fa-thumbs-up"></i> 115 </span>
+                <span><i class="fa-solid fa-share-nodes"></i></span>
+            </p>
+    </div>
+    </div>
+
+
+    <div class="publicationsCSS"> 
+    <div class="article11">
+    <img src="./img/css/css1.png" alt="image compression du fichiers">
+    <p class="descripion">Cet article va vous expliquer comment réduire la taille de vos fichier CSS.L'intérêt de cela est double, en plus de donner un petit coup de main à la planète, vous allez aussi gagner en performance sur vos sites et application web. </p>
+    <a href="https://developpeur-freelance.io/blog/reduire-taille-css/">Réduction de la taille du CSS, compression et suppression de classes inutilisées</a>
+    <p class='social'>
+                <span><i class="fa-regular fa-bookmark"></i></span>
+                <span><i class="fa-regular fa-message"></i> 93 </span>
+                <span><i class="fa-regular fa-thumbs-up"></i> 151 </span>
+                <span><i class="fa-solid fa-share-nodes"></i></span>
+            </p>
+    </div>
+
+<div  class="article12">
+    <img src="./img/css/css2.png" alt="image de l'interface du site CSS Speedrum">
+    <p class="descripion">CSS Speedrum est un mini site permettant de tester vos connaissances CSS. Quelques exercices vous sont proposés, dans lesquels vos compétences en terme de sélecteurs CSS seront mises à rude épreuve. </p>
+    <a href="https://blog.shevarezo.fr/post/2022/01/20/css-speedrun-tester-connaissances-css">CSS Speedrum, un petit jeu pour tester vos connaissances en CSS</a>
+    <p class='social'>
+                <span><i class="fa-regular fa-bookmark"></i></span>
+                <span><i class="fa-regular fa-message"></i> 176 </span>
+                <span><i class="fa-regular fa-thumbs-up"></i> 108 </span>
+                <span><i class="fa-solid fa-share-nodes"></i></span>
+            </p>
+    </div>
+
+<div  class="article13">
+    <img src="./img/css/css3.jpeg" alt="image qui représente un logo css avec grid">
+    <p class="descripion">Pour les gens qui font de la CSS pour habiller leurs pages web et qui souffrent et qui saignent parce que parfois c’est un peu pris de tête, je vous propose qu’aujourd’hui on s’intéresse au concept de Grilles CSS.</p>
+    <a href="https://korben.info/apprendre-grilles-css.html">Comment devenir un expert des grilles CSS ?</a>
+    <p class='social'>
+                <span><i class="fa-regular fa-bookmark"></i></span>
+                <span><i class="fa-regular fa-message"></i> 72 </span>
+                <span><i class="fa-regular fa-thumbs-up"></i> 15 </span>
+                <span><i class="fa-solid fa-share-nodes"></i></span>
+            </p>
+    </div>
+
+<div  class="article14">
+    <img src="./img/css/css4.png" alt="image qui représente les différents frameworks CSS">
+    <p class="descripion">Cet aricle vous guide et vous explique quelle framework et méthodologie CSS choisir</p>
+    <a href="https://www.alsacreations.com/actu/lire/1823-quels-framework-methodologie-css-choisir.html">Quels framework et méthodologie CSS choisir ?</a>
+    <p class='social'>
+                <span><i class="fa-regular fa-bookmark"></i></span>
+                <span><i class="fa-regular fa-message"></i> 43 </span>
+                <span><i class="fa-regular fa-thumbs-up"></i> 83 </span>
+                <span><i class="fa-solid fa-share-nodes"></i></span>
+            </p>
+    </div>
+
+<div  class="article15">
+    <img src="./img/css/css5.png" alt="image qui represente les dimensions d'une image ">
+    <p class="descripion">Dans cet article, nous allons tenter de mieux comprendre comment fonctionne vraiment srcset.Note : cet article a été initialement rédigé en avril 2014 par Geoffrey Crofte. Il a subi une refonte intégrale en mai 2017 pour se mettre à jour et s'étoffer considérablement. </p>
+    <a href="https://www.alsacreations.com/article/lire/1621-responsive-images-srcset.html">Responsive Image : comprendre srcet et sizes</a>
+    <p class='social'>
+                <span><i class="fa-regular fa-bookmark"></i></span>
+                <span><i class="fa-regular fa-message"></i> 98 </span>
+                <span><i class="fa-regular fa-thumbs-up"></i> 151 </span>
+                <span><i class="fa-solid fa-share-nodes"></i></span>
+            </p>
+
+     </div>
+    </div>
+
+</section>
+
+
+
+   
     
     
 
