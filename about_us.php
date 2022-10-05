66331752f10fbf6e3d9d66e8abe80095fc068a72
MaxenceCodeFR <maxence.hattabi@gmail.com>
2022-10-05 17:34:01 +0200

    beaucoup de changement et nouveau fichier content

--- about_us.php
+++ about_us.php
@@ -1,3 +1,7 @@
+<?php
+include 'content_aboutus.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,61 +15,37 @@
 </head>
 
 <body>
-    <header>
-        <?php
-        include '_header.php';
-        ?>
-    </header>
+
+    <?php
+    include '_header.php';
+    ?>
+
     <main>
-        <h1>About Us</h1>
-        <section class="florentv">
-            <h2>Florent Vasseur</h2>
-            <img src="./img/profile_pic_flo.png" alt="Florent Vasseur" />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt magna lacus, et feugiat turpis tempus id. Nulla vestibulum, nibh ac blandit laoreet, massa leo suscipit quam, et dictum purus urna sit amet velit. Quisque sed eros imperdiet, aliquet tellus scelerisque, maximus metus. Etiam eget nisl in neque porttitor finibus. Aenean consectetur nunc sem, vitae porta ipsum cursus eu. Fusce nisi lectus, finibus id dui sed, pulvinar tempus mauris. Duis ut ultricies lorem. Curabitur congue dolor ante, quis interdum nisi sagittis quis. Suspendisse tempor, dui nec commodo consectetur, libero justo molestie nunc, id sodales enim purus lacinia est. Fusce cursus orci nec enim egestas molestie. Donec nec hendrerit metus.</p>
-        </section>
 
-        <div class="unsurdeux">
-            <i class="fa-brands fa-square-instagram"></i><i class="fa-brands fa-square-facebook"></i><i class="fa-brands fa-linkedin"></i><i class="fa-brands fa-square-twitter"></i>
-        </div>
+        <!-- TITRE -->
+        <?php echo $first ?>
 
-        <section class="jessicap">
-            <h2>Jessica Pêcheux</h2>
-            <img src="./img/profile_pic_jess.png" alt="Jessica Pêcheux" />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt magna lacus, et feugiat turpis tempus id. Nulla vestibulum, nibh ac blandit laoreet, massa leo suscipit quam, et dictum purus urna sit amet velit. Quisque sed eros imperdiet, aliquet tellus scelerisque, maximus metus. Etiam eget nisl in neque porttitor finibus. Aenean consectetur nunc sem, vitae porta ipsum cursus eu. Fusce nisi lectus, finibus id dui sed, pulvinar tempus mauris. Duis ut ultricies lorem. Curabitur congue dolor ante, quis interdum nisi sagittis quis. Suspendisse tempor, dui nec commodo consectetur, libero justo molestie nunc, id sodales enim purus lacinia est. Fusce cursus orci nec enim egestas molestie. Donec nec hendrerit metus.</p>
-        </section>
+        <!-- SECTION FLORENT -->
+        <?php echo $sectionFlorent ?>
+        <?php echo $socialMediaFlorent ?>
 
-        <div class="deuxsurdeux">
-            <i class="fa-brands fa-square-instagram"></i><i class="fa-brands fa-square-facebook"></i><i class="fa-brands fa-linkedin"></i><i class="fa-brands fa-square-twitter"></i>
-        </div>
+        <!-- SECTION JESSICA -->
+        <?php echo $sectionJessica ?>
+        <?php echo $socialMediaJessica ?>
 
-        <section class="alexandred">
-            <h2>Alexandre Delmotte</h2>
-            <img src="./img/profile_pic_alexandre.png" alt="Alexandre Delmotte" />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt magna lacus, et feugiat turpis tempus id. Nulla vestibulum, nibh ac blandit laoreet, massa leo suscipit quam, et dictum purus urna sit amet velit. Quisque sed eros imperdiet, aliquet tellus scelerisque, maximus metus. Etiam eget nisl in neque porttitor finibus. Aenean consectetur nunc sem, vitae porta ipsum cursus eu. Fusce nisi lectus, finibus id dui sed, pulvinar tempus mauris. Duis ut ultricies lorem. Curabitur congue dolor ante, quis interdum nisi sagittis quis. Suspendisse tempor, dui nec commodo consectetur, libero justo molestie nunc, id sodales enim purus lacinia est. Fusce cursus orci nec enim egestas molestie. Donec nec hendrerit metus.</p>
-        </section>
+        <!-- SECTION ALEXANDRE -->
+        <?php echo $sectionAlexandre ?>
+        <?php echo $socialMediaAlexandre ?>
 
-        <div class="unsurdeux">
-            <i class="fa-brands fa-square-instagram"></i><i class="fa-brands fa-square-facebook"></i><i class="fa-brands fa-linkedin"></i><i class="fa-brands fa-square-twitter"></i>
-        </div>
-
-        <section class="maxenceh">
-            <h2>Maxence Hattabi</h2>
-            <img src="./img/profile_pic_maxence.png" alt="Maxence Hattabi" />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt magna lacus, et feugiat turpis tempus id. Nulla vestibulum, nibh ac blandit laoreet, massa leo suscipit quam, et dictum purus urna sit amet velit. Quisque sed eros imperdiet, aliquet tellus scelerisque, maximus metus. Etiam eget nisl in neque porttitor finibus. Aenean consectetur nunc sem, vitae porta ipsum cursus eu. Fusce nisi lectus, finibus id dui sed, pulvinar tempus mauris. Duis ut ultricies lorem. Curabitur congue dolor ante, quis interdum nisi sagittis quis. Suspendisse tempor, dui nec commodo consectetur, libero justo molestie nunc, id sodales enim purus lacinia est. Fusce cursus orci nec enim egestas molestie. Donec nec hendrerit metus.</p>
-        </section>
-
-        <div class="deuxsurdeux">
-            <i class="fa-brands fa-square-instagram"></i><i class="fa-brands fa-square-facebook"></i><i class="fa-brands fa-linkedin"></i><i class="fa-brands fa-square-twitter"></i>
-        </div>
+        <!-- SECTION MAXENCE -->
+        <?php echo $sectionMaxence ?>
+        <?php echo $socialMediaMaxence ?>
 
     </main>
 
-
-    <footer>
-        <?php
-        include '_footer.php';
-        ?>
-    </footer>
+    <?php
+    include '_footer.php';
+    ?>
 
 </body>
 
