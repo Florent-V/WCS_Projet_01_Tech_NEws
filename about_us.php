bd446ef93d9ca422be352796854dbe895952d3e0
Florent-Vasseur <vasseurflorent@gmail.com>
2022-10-07 15:25:33 +0200

    ajout favicons

--- about_us.php
+++ about_us.php
@@ -10,6 +10,7 @@ include 'content_aboutus.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tech News to Watch</title>
+    <link rel="shortcut icon" href="./img/logo.png" type="image/x-icon">
     <link rel="stylesheet" href="./style/style_aboutus.css" href="./style/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.2/css/all.css" />
 </head>
--- contact_us.php
+++ contact_us.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tech News to Watch</title>
+    <link rel="shortcut icon" href="./img/logo.png" type="image/x-icon">
     <link rel="stylesheet" href="./style/style.css">
     <link rel="stylesheet" href="./style/contact.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.2/css/all.css" />
@@ -48,9 +49,9 @@ include '_header.php';
             <textarea id="messageForm" name="Message" placeholder="Saisir votre message" ></textarea><br />
             <!--<input type="submit" class="buttonForm">!-->
               <button>
-              <span>
-                Envoyer
-              </span>
+                <span>
+                  Envoyer
+                </span>
             </button>
         </div> 
         
